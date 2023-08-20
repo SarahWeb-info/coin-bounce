43c7b82020e1a0ca83eaf17e80eb41673f8b8661
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-08-20 23:33:19 +0500

    stuck here with the video somewhere in 1:53 skipped . Going back to code with harry

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const dbConnect = require('./database/index');
 const {PORT} = require('./config/index');
+const router = require('./routes/index');
 const app = express();
-// const port = 5000;
+app.use(express.json());
+app.use(router);
 dbConnect();
 app.get('/', (req, res) => {
     res.json({msg : 'Hello World123!'});
   })
+  app.use(errorHandler);  
 app.listen(PORT, () => {
   console.log(`Example app listening on port `)
-})
+}) 
